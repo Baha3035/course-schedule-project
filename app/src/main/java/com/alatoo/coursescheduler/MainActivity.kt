f1a5f273e413174316efa0cb7a7cfa503497193b
Baha3035 <batmaaanpls@gmail.com>
2023-12-09 11:14:02 +0600

    finish ui

--- app/src/main/java/com/alatoo/coursescheduler/MainActivity.kt
+++ app/src/main/java/com/alatoo/coursescheduler/MainActivity.kt
@@ -2,6 +2,8 @@ package com.alatoo.coursescheduler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.alatoo.coursescheduler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navController.navController)
     }
 }
 
--- app/src/main/java/com/alatoo/coursescheduler/RegistrationActivity.kt
+++ app/src/main/java/com/alatoo/coursescheduler/RegistrationActivity.kt
@@ -1,11 +1,21 @@
 package com.alatoo.coursescheduler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alatoo.coursescheduler.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.registerBtn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
